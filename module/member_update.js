73bdf1bfb9e40afabe3a1aae5c0516d458a2871d
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-10-10 18:25:59 +0900

    UPDATE구현중

--- module/member_update.js
+++ module/member_update.js
@@ -16,7 +16,11 @@ import { db } from "./firebase/firebase_config.js";
 try {
 
   /* make a select query */
-  var q = await doc(db, "albums", 'member1');
+  const url_str = window.location.href;
+  const url = new URL(url_str);
+  const id = url.searchParams.get("id");
+
+  var q = await doc(db, "team", id);
   console.log(q)
   /* request a query */
   var querySnapshot = await getDoc(q);
@@ -30,35 +34,64 @@ try {
   let strength = mem_info_row["strength"];
   let work_style = mem_info_row["work_style"];
   let blog_url = mem_info_row["blog_url"];
-  let image_url = " ";
+  let image_url = mem_info_row["image_url"];
 
   let temp_html = `
-        <div class="form-floating mb-2">
-          <input type="TEXT" class="form-control" id="u_name" placeholder="NAME" value="${name}">
-          <label for="floatingInput">NAME</label>
+      <div class="d-flex align-items-center mb-4">
+      <div class="p-5" id="card-content" style="width:450px">
+        <h2 class="fw-bolder">Member Name 1</h2>
+        <div>
+          <!-- 카드 내용 -->
+          <div class="form-floating mb-2">
+            <input type="TEXT" class="form-control" id="u_name" placeholder="NAME" value="${name}">
+            <label for="floatingInput">NAME</label>
+          </div>
+          <div class="form-floating mb-2">
+            <input type="TEXT" class="form-control" id="u_mbti" placeholder="MBTI" value="${mbti}">
+            <label for="floatingPassword">MBTI</label>
+          </div>
+          <div class="form-floating mb-2">
+            <input type="TEXT" class="form-control" id="u_strength" placeholder="STRENGTH" value="${strength}">
+            <label for="floatingInput">STRENGTH</label>
+          </div>
+          <div class="form-floating mb-2">
+            <input type="TEXT" class="form-control" id="u_workStyle" placeholder="WORK_STYLE" value="${work_style}">
+            <label for="floatingPassword">WORK_STYLE</label>
+          </div>
+          <div class="form-floating mb-2">
+            <input type="EMAIL" class="form-control" id="u_blogUrl" placeholder="BLOG_URI" value="${blog_url}">
+            <label for="floatingPassword">BLOG_URI</label>
+          </div>
+          <div class="form-floating mb-2">
+            <input type="TEXT" class="form-control" id="u_tmi" placeholder="TMI" value="${tmi}">
+            <label for="floatingPassword">TMI</label>
+          </div>
         </div>
-        <div class="form-floating mb-2">
-          <input type="TEXT" class="form-control" id="u_mbti" placeholder="MBTI" value="${tmi}">
-          <label for="floatingPassword">TMI</label>
-        </div>
-        <div class="form-floating mb-2">
-          <input type="TEXT" class="form-control" id="u_strength" placeholder="STRENGTH" value="${mbti}">
-          <label for="floatingInput">STRENGTH</label>
-        </div>
-        <div class="form-floating mb-2">
-          <input type="TEXT" class="form-control" id="u_workStyle" placeholder="WORK_STYLE" value="${strength}">
-          <label for="floatingPassword">WORK_STYLE</label>
-        </div>
-        <div class="form-floating mb-2">
-          <input type="EMAIL" class="form-control" id="u_blogUri" placeholder="abd@nbcamp.com" value="${work_style}">
-          <label for="floatingPassword">BLOG_URI</label>
-        </div>
-        <div class="form-floating mb-2">
-          <input type="TEXT" class="form-control" id="u_tmi" placeholder="TMI" value="${blog_url}">
-          <label for="floatingPassword">TMI</label>
-        </div>
-        `
-        $("#updateContent").append(temp_html);
+      </div>
+      <img
+        class="img-fluid"
+        src="https://dummyimage.com/300x400/343a40/6c757d"
+        alt="..."
+        value="${image_url}"
+      />
+    </div>
+    <div class="d-flex justify-content-end mb-4"
+    >
+      <div class="d-flex justify-content-between"
+      >
+        <!-- link to ./member_modify.html -->
+        <a
+          class="btn btn-primary mx-2 px-4 py-3 hoverButton"
+          id="updateBtn"
+        >
+          <div
+            class="d-inline-block bi bi-pencil-square me-0"
+          ></div>
+        </a>
+      </div>
+    </div>
+  `
+  $("#updateContent").append(temp_html);
 } catch (error) {
   console.log(error)
 }
@@ -160,33 +193,32 @@ $("#updateBtn").click(async function () {
   // 3. 없으면 새로 추가
   // 4. merge값이 true이면 데이터가 없다면 추가, 있다면 update처럼 작용
 
-  // const url_str = window.location.href;
-  // const url = new URL(url_str);
-  // const id = url.searchParams.get("id");
-
-  const team = doc(db, 'albums', 'member1');
+  const url_str = window.location.href;
+  const url = new URL(url_str);
+  const id = url.searchParams.get("id");
+  const team = doc(db, 'team', id);
 
   let name = $('#u_name').val();
   let mbti = $('#u_mbti').val();
   let strength = $('#u_strength').val();
   let work_style = $('#u_workStyle').val();
-  let blog_uri = $('#u_blogUri').val();
+  let blog_url = $('#u_blogUrl').val();
   let tmi = $('#u_tmi').val();
-
-  setDoc(team, {
+  
+  await setDoc(team, {
     name: name,
     mbti: mbti,
     strength: strength,
     work_style: work_style,
-    blog_uri: blog_uri,
+    blog_url: blog_url,
     tmi: tmi
-  }, { merge: false });
+  }, { merge: true });
 
-  const MemberUpdate = doc(db, "albums", "member1");
+  const MemberUpdate = doc(db, "team", id);
 
   console.log(MemberUpdate)
 
   alert("수정되었습니다.");
-
+  location.href = "team_main.html";
 });
 
