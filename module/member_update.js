e146cef296529f49a67ad3c201dd88bcf7163810
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-10-10 14:39:18 +0900

    DELETE구현완료/UPDATE구현중

--- module/member_update.js
+++ module/member_update.js
@@ -7,6 +7,7 @@ import {
   doc,
   query,
   where,
+  updateDoc,
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { db } from "./firebase/firebase_config.js";
 
@@ -15,95 +16,47 @@ $("#updatePageBtn").click(async function () {
   location.href = "member_update.html"
 
   // 클릭 시 ID값 받아오기
-  const url_str = window.location.href;
-  const url = new URL(url_str);
-  const id = url.searchParams.get("id");
+  // const url_str = window.location.href;
+  // const url = new URL(url_str);
+  // const id = url.searchParams.get("id");
 
-  console.log("id : " + id)
-
-  // 자리에 값 뿌려주기
-  const temp_html = `
-      <div class="form-floating mb-2">
-        <input type="TEXT" class="form-control" id="u_name" placeholder="NAME">
-        <label for="floatingInput">NAME</label>
-      </div>
-      <div class="form-floating mb-2">
-        <input type="TEXT" class="form-control" id="u_mbti" placeholder="MBTI">
-        <label for="floatingPassword">MBTI</label>
-      </div>
-      <div class="form-floating mb-2">
-        <input type="TEXT" class="form-control" id="u_stength" placeholder="STRENGTH">
-        <label for="floatingInput">STRENGTH</label>
-      </div>
-      <div class="form-floating mb-2">
-        <input type="TEXT" class="form-control" id="u_workStyle" placeholder="WORK_STYLE">
-        <label for="floatingPassword">WORK_STYLE</label>
-      </div>
-      <div class="form-floating mb-2">
-        <input type="EMAIL" class="form-control" id="u_blogUri" placeholder="abd@nbcamp.com">
-        <label for="floatingPassword">BLOG_URI</label>
-      </div>
-      <div class="form-floating mb-2">
-        <input type="TEXT" class="form-control" id="u_tmi" placeholder="TMI">
-        <label for="floatingPassword">TMI</label>
-      </div>
-  `
-
+  // console.log("id : " + id)
 
 });
 
 // 정보수정 후 버튼 클릭 시
 $("#updateBtn").click(async function () {
+  //setDoc
+  // 1. 해당컬렉션에서 id값을 가진 도큐먼트값을 데이터로 수정
+  // 2. 이미 도큐먼트가 존재하면 수정
+  // 3. 없으면 새로 추가
+  // 4. merge값이 true이면 데이터가 없다면 추가, 있다면 update처럼 작용
 
-  // // 데이터 입력 값
-  // const name = $("#u_name").val();
-  // const mbti = $("#u_mbti").val();
-  // const strength = $("#u_strength").val();
-  // const work_style = $("#u_workStyle").val();
-  // const blog_url = $("#u_blogUri").val();
-  // const tmi = $("#u_tmi").val();
+  // const url_str = window.location.href;
+  // const url = new URL(url_str);
+  // const id = url.searchParams.get("id");
 
-  // console.log(name);
-  // console.log(mbti);
-  // console.log(strength);
-  // console.log(work_style);
-  // console.log(blog_url);
-  // console.log(tmi);
- 
-  // update query
-  const frankDocRef = db.collection("team").doc("ATytFZE4ixmSeZn4m3mg");
-  frankDocRef.set({
-    name: name,
-    mbti: mbti,
-    strength: strength,
-    work_style: work_style,
-    blog_url: blog_url,
-    tmi: tmi
-  });
+  const team = doc(db, 'team', 'member1');
 
-  // To update age and favorite color:
-  db.collection("team").doc("ATytFZE4ixmSeZn4m3mg").update({
-    name: name,
-    mbti: mbti,
-    strength: strength,
-    work_style: work_style,
-    blog_url: blog_url,
-    tmi: tmi
-  })
-    .then(function () {
-      console.log("Document successfully updated!");
-    });
+  let name = $('#u_name').val();
+  let mbti = $('#u_mbti').val();
+  let strength = $('#u_strength').val();
+  let work_style = $('#u_workStyle').val();
+  let blog_uri = $('#u_blogUri').val();
+  let tmi = $('#u_tmi').val();
 
-  // const memberUpdate = await addDoc(collection(db, "team"), {
-  //   name: name,
-  //   mbti: mbti,
-  //   strength: strength,
-  //   work_style: work_style,
-  //   blog_url: blog_url,
-  //   tmi: tmi,
+  setDoc(team, { name: name,
+                 mbti: mbti,
+                 strength: strength,
+                 work_style: work_style,
+                 blog_uri: blog_uri,
+                 tmi: tmi}, { merge: false });
 
-  // });
-  console.log("수정되었습니다.");
+  const MemberUpdate = doc(db, "team", "member1");
+  
+  console.log(MemberUpdate)
+  
+  alert("수정되었습니다.");
 
 });
 
